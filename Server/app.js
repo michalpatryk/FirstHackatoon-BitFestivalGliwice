a21eee42a29a8d779d150a41c2b9074606bfb2d9
blazejhanzel <blazej.hanzel@gmail.com>
2019-11-28 16:45:59 +0100

    mongodb connection

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyparser = require('body-parser')
 
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@sekcja-spalonych-tostow-ppujf.mongodb.net/bitfestival?retryWrites=true&w=majority');
+
+const bubbleScheme = {
+    login: String
+}
+
+const Bubble = mongoose.model("Bubble", bubbleScheme);
 
 app.use(bodyparser.json());
 
@@ -11,6 +20,14 @@ app.get('/', (req, res) =>
     res.send("helloword00")
 )
 
+app.get('/addBubble', (req, res) => {
+
+    const bubble = new Bubble({
+        login: req.body.login
+    })
+    bubble.save()
+    res.json({ accepted: true })
+})
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
